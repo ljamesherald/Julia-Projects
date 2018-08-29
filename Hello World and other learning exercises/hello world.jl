376298ec5d61c107e270f66258cb0ff24e5031fe
ljamesherald <ljamesherald@gmail.com>
2018-08-29 18:58:13 -0400

    First steps
    This is a basic hello world program where I will be creaticly learning how to ccode with Julia. I will also be using this opprotunity to familiarize myself with Atom and github desktop.

---
+++ Hello World and other learning exercises/hello world.jl
@@ -0,0 +1,7 @@
+#=
+    so this is my basic hello world program for learning julia(please dont laugh at it)
+    ya boi looks forward to seeing what all julia can do and hopes to unloc its full power as language for data science)
+=#
+
+println("sah dude")
+println("its ya boi")
