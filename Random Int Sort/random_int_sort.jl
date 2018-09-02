8094e60ec873caf128f2b6865d9be4a32fa1441c
ljamesherald <ljamesherald@gmail.com>
2018-09-02 14:03:28 -0400

    Timed bubble sort

--- Random Int Sort/random_int_sort.jl
+++ Random Int Sort/random_int_sort.jl
@@ -27,17 +27,23 @@ random_array = rand(0:100, n)
 #this reporesents the run time after building and populating the array
 run_time_after_array = time_ns()
 
-
 println(length(random_array))
 println(random_array)
 # this fuction prints the total time it took to declare, and populate the array
 println(run_time_after_array - run_time_before_array)
 
+#this is my bubble sort algortithm, which is pretty neat
+
+#this reporesents the run time before sorting the array
+run_time_before_bubble_sort = time_ns()
     for _ in 2:length(random_array), j in 1:length(random_array)-1
         if random_array[j] > random_array[j+1]
             random_array[j], random_array[j+1] = random_array[j+1], random_array[j]
         end
     end
-
+#this reporesents the run time after sorting the array
+run_time_after_bubble_sort = time_ns()
 
 println((random_array))
+# this fuction prints the total time it took to declare, and populate the array
+println(run_time_after_bubble_sort - run_time_before_bubble_sort)
