83fdc6b573efc9c8687f4c3b626a53d876edecaa
ljamesherald <ljamesherald@gmail.com>
2018-09-02 19:39:43 -0400

    I added a selection sort

--- Random Int Sort/random_int_sort.jl
+++ Random Int Sort/random_int_sort.jl
@@ -16,34 +16,60 @@
 =#
 
 #n reporesents the number of elements in my arra
-n = 100
+n = 10000
 
 #this reporesents the run time before building and populating the array
 run_time_before_array = time_ns()
-
 # the rand function populates an array with random integers with a range of (0,100) and a size of n elements
-random_array = rand(0:100, n)
-
+random_array = rand(0:10000, n)
 #this reporesents the run time after building and populating the array
 run_time_after_array = time_ns()
 
-println(length(random_array))
+#this clones the array so i can preform another sorting algorithm on the same ccollecction
+random_array_for_selection_sort = random_array
+
+println("number of random intigers in array: ", n)
 println(random_array)
 # this fuction prints the total time it took to declare, and populate the array
-println(run_time_after_array - run_time_before_array)
-
-#this is my bubble sort algortithm, which is pretty neat
+println("time to create/populate array: ", run_time_after_array - run_time_before_array, " ns")
+println()
+println("bubble sorting array")
+println()
 
 #this reporesents the run time before sorting the array
 run_time_before_bubble_sort = time_ns()
-    for _ in 2:length(random_array), j in 1:length(random_array)-1
-        if random_array[j] > random_array[j+1]
-            random_array[j], random_array[j+1] = random_array[j+1], random_array[j]
-        end
+#this is my bubble sort algortithm, which is pretty neat
+for _ in 2:length(random_array), j in 1:length(random_array)-1
+    if random_array[j] > random_array[j+1]
+        random_array[j], random_array[j+1] = random_array[j+1], random_array[j]
     end
+end
 #this reporesents the run time after sorting the array
 run_time_after_bubble_sort = time_ns()
 
-println((random_array))
+
+println(random_array)
 # this fuction prints the total time it took to declare, and populate the array
-println(run_time_after_bubble_sort - run_time_before_bubble_sort)
+println("time to bubble sort array: ", run_time_after_bubble_sort - run_time_before_bubble_sort, " ns")
+println()
+
+println("Selection sorting array")
+println()
+#this reporesents the run time before sorting the array
+run_time_before_selection_sort = time_ns()
+len = length(random_array_for_selection_sort)
+if len < 2 return random_array_for_selection_sort end
+for i in 1:len-1
+    lmin, j = findmin(random_array_for_selection_sort[i+1:end])
+    if lmin < random_array_for_selection_sort[i]
+        random_array_for_selection_sort[i+j] = random_array_for_selection_sort[i]
+        random_array_for_selection_sort[i] = lmin
+    end
+end
+#this reporesents the run time after sorting the array
+run_time_after_selection_sort = time_ns()
+
+println(random_array_for_selection_sort)
+# this fuction prints the total time it took to declare, and populate the array
+println("time to bubble sort array: ", run_time_after_selection_sort - run_time_before_selection_sort, " ns")
+println()
