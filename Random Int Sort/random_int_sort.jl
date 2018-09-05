c6e831caed89dec66f4fee629e4e0c365726f783
Logan Herald <ljherald@localhost.localdomain>
2018-09-05 15:14:27 -0400

    Manage to clean some unused lines

--- Random Int Sort/random_int_sort.jl
+++ Random Int Sort/random_int_sort.jl
@@ -3,16 +3,7 @@
     CS3800
     Programing Project 1
     Random Int Genorator and sorter
-    due 2/5/2016
-    This program calculates the volume
-    of a cylinder, in cubic inches,
-    given user intered height and
-    diameter in inches
-=#
-#=
-    this is the decloration of my array.
-    Because julia is a dynamicly typed language,
-    i dont need to declare my variable type.
+    due 9/6/2018
 =#
 
 #n reporesents the number of elements in my arra
